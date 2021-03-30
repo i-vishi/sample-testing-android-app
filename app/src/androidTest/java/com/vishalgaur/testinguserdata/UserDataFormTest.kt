a831f995d20d99b7ea1444462c9fcf573b55c9ed
i-vishi <rr.vishalgaur@gmail.com>
2021-03-30 23:48:16 +0530

    added some instrumentation tests

--- app/src/androidTest/java/com/vishalgaur/testinguserdata/UserDataFormTest.kt
+++ app/src/androidTest/java/com/vishalgaur/testinguserdata/UserDataFormTest.kt
@@ -1,5 +1,6 @@
 package com.vishalgaur.testinguserdata
 
+import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.matcher.ViewMatchers.withId
@@ -10,6 +11,8 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -17,11 +20,38 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class UserDataFormTest {
+
+	@Before
+	fun setup() {
+		launchFragmentInContainer<InputFragment>(themeResId = R.style.Theme_TestingUserData)
+	}
+
 	@Test
 	fun useAppContext() {
 		// Context of the app under test.
 		val appContext = InstrumentationRegistry.getInstrumentation().targetContext
 		assertEquals("com.vishalgaur.testinguserdata", appContext.packageName)
+	}
+
+	@Test
+	fun userCanEnterName() {
+		onView(withId(R.id.input_name_edit_text)).perform(typeText("Vishal Gaur"))
+	}
+
+	@Test
+	fun userCanEnterEmail() {
+		onView(withId(R.id.input_email_edit_text)).perform(typeText("vishal123456@somemail.com"))
+	}
+
+	@Test
+	fun userCanEnterMobile() {
+		onView(withId(R.id.input_mobile_edit_text)).perform(typeText("8989895656"))
+	}
+
+	@Test
+	fun userCanEnterBio() {
+		onView(withId(R.id.input_bio_edit_text))
+				.perform(typeText("some random bio with multiple      spaces and  reicvb  ikejrbv."))
 	}
 }
